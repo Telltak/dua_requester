20583c2bf0e64f4127326578247b1ca811a863c3
Telltak <236608+Telltak@users.noreply.github.com>
2025-07-29 17:04:10 +0100

    add dynamo definition (#1)

---
+++ infrastructure/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+
+  required_version = ">= 1.10.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "telltak-terraform-state"
+    key          = "dua_requester"
+    region       = "eu-west-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      Application = "dua_requester"
+    }
+  }
+}
